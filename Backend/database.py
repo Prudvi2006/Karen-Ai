099c66273c909dd9ee2e7fff6f605001cc46243a
Prudvi2006 <thallapallyprudviteja@gmail.com>
2026-07-21 16:57:59 +0530

    Database

--- Backend/database.py
+++ Backend/database.py
@@ -1,11 +1,33 @@
 # pyrefly: ignore [missing-import]
 from pymongo import MongoClient
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+# client = MongoClient(os.getenv("MONGODB_URI"))
+# db = client["chatbot_db"]
+# chats_collection = db["chats"]
+# messages_collection = db["messages"]
+# users_collection = db["users"]
+
+
+from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-load_dotenv()
-client = MongoClient(os.getenv("MONGODB_URI"))
-db = client["chatbot_db"]
-chats_collection = db["chats"]
-messages_collection = db["messages"]
-users_collection = db["users"]
 
+load_dotenv()
+
+uri = os.getenv("MONGODB_URI")
+
+print("=" * 50)
+print("Mongo URI:", uri)
+print("=" * 50)
+
+client = MongoClient(uri)
+
+print(client.admin.command("ping"))
+
+db = client["chatbot_db"]
+
+users_collection = db["users"]
+messages_collection = db["messages"]
+chats_collection = db["chats"]
